1179755b610e1d47cff151f7ff1a30683538e9d4
Aluno <aluno@fiern.local>
2019-03-18 16:39:39 -0300

    adicionar

---
+++ aula-14_03/js/Exercicios.js
@@ -0,0 +1,11 @@
+let n1 = parseFloat(prompt("Qual e o seu nome:"));
+let n2 = parseFloat(prompt("Quantos anos vc tem:"));
+let n3 = parseFloat(prompt("Digite o numero:")); 
+let n4 = parseFloat(prompt("Quantos graos de racao o animal come por dia:"));
+let n5 = parseFloat(prompt("Quantos graos existe no estoque:"));
+let n6 = parseFloat(prompt("Quanto custa o cigarro:"));
+let n7 = parseFloat(prompt("Quantos anos ele fuma:"));
+
+let  = (n1+n2+n3+n4+n5+n6+n7) / 3;
+
+alert (media);
