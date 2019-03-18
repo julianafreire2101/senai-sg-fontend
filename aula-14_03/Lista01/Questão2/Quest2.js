2118471addf3c5e4eac21aac0d169b72057ce4fa
Aluno <aluno@fiern.local>
2019-03-18 16:40:45 -0300

    adicionar

---
+++ aula-14_03/Lista01/Questão2/Quest2.js
@@ -0,0 +1,4 @@
+let n1 = parseFloat(prompt("Digite Um Numero"));
+let media = (n1);
+
+alert("O número informado foi " + media);
---
+++ aula-14_03/Lista01/Questão4/Quest4.js
@@ -0,0 +1,19 @@
+var n1 = parseFloat(window.prompt("Digite a nota 1"));
+var n2 = parseFloat(window.prompt("Digite a nota 2"));
+var n3 = parseFloat(window.prompt("Digite a nota 3"));
+
+var media = (n1 + n2 + n3) /3;
+
+window.alert("A média é : "+ media);
+
+let nota = prompt("Digite sua nota");
+
+nota = parseFloat(nota);
+
+if (nota >= 7) {
+    alert("APROVADO");
+} else if (nota >= 5) {
+    alert("RECUPERAÇÃO");
+} else {
+    alert("REPROVADO");
+}
