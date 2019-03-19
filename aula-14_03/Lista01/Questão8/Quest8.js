f72629fbf62a57889145def95ab80b16ebe15943
Aluno <aluno@fiern.local>
2019-03-19 16:55:29 -0300

    adicionar

---
+++ aula-14_03/Lista01/Questão8/Quest8.js
@@ -0,0 +1,54 @@
+let qh = parseFloat(prompt("Quanto você ganha por hora: "));
+let ht = parseFloat(prompt("Quantas horas você trabalhou: "));
+
+let salarioB = (qh * ht);
+
+let ir = (11/100.0 * salarioB);
+let inss = (8/100.0 * salarioB);
+let sindicato = (5/100.0 * salarioB);
+
+let vt = (ir + inss + sindicato);
+let salarioL = (salarioB - vT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//qH = input("Quanto você guanha por hora: ")
+//hT = input("Quantas horas você trabalhou: ")
+
+//salarioB = qH * hT
+
+//ir = (11/100.0 * salarioB)
+//inss = (8/100.0 * salarioB)
+//sindicato = (5/100.0 * salarioB)
+
+//vT = ir + inss + sindicato
+//salarioL = salarioB - vT
+
+//print 'Seu salário bruto é',salarioB
+
+//print 'Valor dos impostos:'
+//print 'IR: ',ir
+//print 'INSS: ',inss
+//print 'Sindicato: ',sindicato
+
+//rint 'Seu salário liquido é: ',salarioL
