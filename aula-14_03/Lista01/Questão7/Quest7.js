1b64632109e978c9fd39f70e592942ce20977a95
Aluno <aluno@fiern.local>
2019-03-19 16:17:48 -0300

    adicionar

---
+++ aula-14_03/Lista01/Questão7/Quest7.js
@@ -0,0 +1,8 @@
+let altura = parseFloat(prompt("Digite a altura do quadrado em metros: "));
+let largura = parseFloat(prompt("Digite a largura do quadrado em metros: "));
+
+let altura = (largura * 100);
+
+alert(altura + " Metros equivalem a " + altura + " do quadrado");
+
+alert(" O dobro da area é: " + altura);
